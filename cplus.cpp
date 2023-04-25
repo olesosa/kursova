3f3c67e762329c06a54a2fc5b47fdab05a583133
Олесь <102287993+olesosa@users.noreply.github.com>
2023-04-25 23:03:54 +0300

    Create cplus.cpp

---
+++ cplus.cpp
@@ -0,0 +1,393 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Base
+{
+    virtual void print() = 0; // віртуальна функція
+};
+
+class MyException
+{
+public:
+    MyException(const string& msg) : msg_(msg) {}
+    ~MyException() {}
+
+    string getMessage() const { return(msg_); }
+private:
+    string msg_;
+};
+
+class Auto : public Base // клас АВТОМОБІЛЬ
+{
+protected:
+    string number; // державний номер автомобіля
+    string brand; // марка автомобіля
+    int loadCapacity; // вантажопідйомність
+    float fuel; // норма витрат пального
+    string location; // місце приписки
+
+public:
+    Auto() // конструктор без параметрів
+    {
+        number = "AA1234AA";
+        brand = "Audi";
+        loadCapacity = 1000;
+        fuel = 10.0;
+        location = "Львiв";
+    }
+    // конструктор з параметрами
+    Auto(string number, string brand, int loadCapacity, float fuel, string location)
+    {
+        this->number = number;
+        this->brand = brand;
+        this->loadCapacity = loadCapacity;
+        this->fuel = fuel;
+        this->location = location;
+    }
+
+    ~Auto() {}
+
+    void print() override
+    {
+        cout << "Brand" << brand << endl;
+    }
+
+    // методи встановлення значень полів
+    void setNumber(string number) { this->number = number; }
+    void setBrand(string brand) { this->brand = brand; }
+    void setLoadCapacity(int loadCapacity) { this->loadCapacity = loadCapacity; }
+    void setFuel(float fuel) { this->fuel = fuel; }
+    void setLocation(string location) { this->location = location; }
+
+    // методи виведення значень полів
+    string getNumber() const  { return number; }
+    string getBrand() const { return brand; }
+    int getLoadCapacity() const { return loadCapacity; }
+    float getFuel() const { return fuel;}
+    string getLocation() const { return location; }
+
+    // перевантаження операцій
+    void operator() (string number, string brand, int loadCapacity, float fuel, string location) 
+    {
+        this->number = number;
+        this->brand = brand;
+        this->loadCapacity = loadCapacity;
+        this->fuel = fuel;
+        this->location = location;
+    }
+
+    Auto& operator= (const Auto& other) 
+    {
+        if (this == &other) 
+        {
+            return *this;
+        }
+
+        this->number = other.number;
+        this->brand = other.brand;
+        this->loadCapacity = other.loadCapacity;
+        this->fuel = other.fuel;
+        this->location = other.location;
+
+        return *this;
+    }
+
+    friend istream& operator>> (istream& in, Auto& automobile) 
+    {
+        cout << "Enter state number ";
+        in >> automobile.number;
+        cout << "Enter brand ";
+        in >> automobile.brand;
+        cout << "Enter load capacity ";
+        in >> automobile.loadCapacity;
+        cout << "Enter fuel consumption ";
+        in >> automobile.fuel;
+        cout << "Enter registration location ";
+        in >> automobile.location;
+
+        return in;
+    }
+
+    friend ostream& operator<< (ostream& out, const Auto& automobile) 
+    {
+        out << "State number " << automobile.number << endl;
+        out << "Brand " << automobile.brand << endl;
+        out << "Load capacity " << automobile.loadCapacity << endl;
+        out << "Fuel consumption " << automobile.fuel << endl;
+        out << "Location " << automobile.location << endl;
+
+        return out;
+    }
+};
+
+class Driver : public Base // клас ВОДІЙ
+{
+protected:
+    string surname; // прізвище
+    string initials; // ініціали
+    int id; // табельний номер
+    double salary; // оклад
+
+public:
+    Driver() // конструктор без параметрів
+    {
+        surname = "Коваль";
+        initials = "A. O.";
+        id = 1111;
+        salary = 150.0;
+    }
+    // конструктор з параметрами
+    Driver(string surname, string initials, int id, double salary) 
+    {
+        this->surname = surname;
+        this->initials = initials;
+        this->id = id;
+        this->salary = salary;
+    }
+
+    ~Driver() {}
+
+    void print() override
+    {
+        cout << "Driver name " << surname << initials << endl;
+    }
+
+    // методи встановлення значень полів
+    void setSurname(string surname) { this->surname = surname; }
+    void setInitials(string initials) { this->initials = initials; }
+    void setId(int id) { this->id = id; }
+    void setSalary(double salary) { this->salary = salary; }
+
+    // методи виведення значень полів
+    string getSurname() const { return surname; }
+    string getInitials() const { return initials; }
+    int getId() const { return id; }
+    double getSalary() const { return salary; }
+
+    // перевантаження операцій
+    void operator() (string surname, string initials, int id, double salary) {
+        this->surname = surname;
+        this->initials = initials;
+        this->id = id;
+        this->salary = salary;
+    }
+
+    Driver& operator= (const Driver& other) 
+    {
+        if (this == &other) return *this;
+
+        surname = other.surname;
+        initials = other.initials;
+        id = other.id;
+        salary = other.salary;
+
+        return *this;
+    }
+
+    friend ostream& operator<< (ostream& out, const Driver& driver)
+    {
+        out << "Surname " << driver.surname << endl;
+        out << "Initials " << driver.initials << endl;
+        out << "ID " << driver.id << endl;
+        out << "Salary " << driver.salary << endl;
+
+        return out;
+    }
+
+    friend istream& operator>> (istream& in, Driver& driver)
+    {
+        cout << "Enter surname ";
+        in >> driver.surname;
+        cout << "Enter initials ";
+        in >> driver.initials;
+        cout << "Enter ID ";
+        in >> driver.id;
+        cout << "Enter salary ";
+        in >> driver.salary;
+
+        return in;
+    }
+};
+
+class Trip : public Auto, public Driver // клас РЕЙС з множинним успадкуванням
+{
+protected:
+    string date; // дата рейсу
+    string endpoint; // кінцевий пункт рейсу,
+    double kilometrage; // кілометраж
+    int transportedTons; // перевезено тонн
+    int fuelForATrip; // розхід пального за рейс
+    Auto* automobile; // автомобіль, що використовується на поїздку
+    Driver* driver; // водій, який здійснює поїздку
+
+public:
+    Trip() : Auto(), Driver() // конструктор без параметрів
+    {
+        date = "17.02.2023";
+        endpoint = "Київ";
+        kilometrage = 1000.0;
+        transportedTons = 5;
+        fuelForATrip = 50;
+        automobile = nullptr;
+        driver = nullptr;
+    }
+    // конструктор з параметрами
+    Trip(string date, string endpoint, double kilometrage, int transportedTons, int fuelForATrip, const Auto* automobile, const Driver* driver)
+    {
+        this->date = date;
+        this->endpoint = endpoint;
+        this->kilometrage = kilometrage;
+        this->transportedTons = transportedTons;
+        this->fuelForATrip = fuelForATrip;
+        this->automobile;
+        this->driver;
+    }
+    ~Trip() 
+    {
+        delete automobile;
+        delete driver;
+    }
+
+    void print() override
+    {
+        cout << "Place of Endpoint " << endpoint << endl;
+    }
+
+    // Методи встановлення значень полів даних
+    void setDate(string date) { this->date = date; }
+    void setEndpoint(string endpoint) { this->endpoint = endpoint; }
+    void setKilometrage(int kilometrage) { this->kilometrage = kilometrage; }
+    void setTransportedTons(int transportedTons) { this->transportedTons = transportedTons; }
+    void setFuelForATrip(int fuelForATrip) { this->fuelForATrip = fuelForATrip; }
+
+    // методи виведення значень полів
+    string getDate() const { return date; }
+    string getEndpoint() const { return endpoint; }
+    double getKilometrage() const { return kilometrage; }
+    int getTransportedTons() const { return transportedTons; }
+    int getFuelForATrip() const { return fuelForATrip; }
+};
+
+class Timetable : private Trip // клас РОЗКЛАД
+{
+private:
+    int size; // розмір масиву
+    Trip* arr; // масив обєктів класу РЕЙС
+
+public:
+    Timetable(int size) // конструктор класу
+    {
+        this->size = size;
+        arr = new Trip[size];
+    }
+
+    ~Timetable() // деструктор
+    {
+        delete arr;
+    }
+
+    double VolumeOfTransportation()
+    {
+        int vol = 0;
+        for (Trip* it = begin(); it != end(); ++it)
+        {
+            vol += it->getTransportedTons();
+        }
+        cout << "Total volume of transportation " << vol << endl;
+    }
+
+    double TotalFuel()
+    {
+        int total = 0;
+        for (Trip* it = begin(); it != end(); ++it)
+        {
+            total += it->getFuel();
+        }
+        cout << "Total fuel " << total << endl;
+    }
+
+    Trip* begin()
+    {
+        return arr;
+    }
+
+    Trip* end()
+    {
+        return arr + size;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+
+    cout << "============================================" << endl;
+    cout << "|         Осадчук Олесь Леонiдович         |" << endl;
+    cout << "============================================" << endl;
+    cout << endl;
+
+    bool a = true;
+    while (a == true)
+    {
+        cout << "============================================" << endl;
+        cout << "| Виберiть дiю                             |" << endl;
+        cout << "|                                          |" << endl;
+        cout << "| 1 - Створити обєкт касу Авто             |" << endl;
+        cout << "| 2 - Створити обєкт касу Водій            |" << endl;
+        cout << "| 3 -                                      |" << endl;
+        cout << "| 4 -                                      |" << endl;
+        cout << "| 5 -                                      |" << endl;
+        cout << "| 6 - Вихiд                                |" << endl;
+        cout << "============================================" << endl;
+
+        int b; cin >> b;
+
+        if (b == 1)
+        {
+            cout << "============================================" << endl;
+            cout << "| Бажаєте ввести данi                      |" << endl;
+            cout << "|                                          |" << endl;
+            cout << "| 1 - так                                  |" << endl;
+            cout << "| 2 - нi                                   |" << endl;
+            cout << "============================================" << endl;
+
+            int c; cin >> c;
+
+            if (c == 1)
+            {
+
+            }
+            else if (c == 2)
+            {
+                Auto a1 = Auto();
+
+            }
+
+        }
+        else if (b == 2)
+        {
+            cout << "============================================" << endl;
+        }
+        else if (b == 3)
+        {
+            cout << "============================================" << endl;
+        }
+        else if (b == 4)
+        {
+            cout << "============================================" << endl;
+        }
+        else if (b == 5)
+        {
+            cout << "============================================" << endl;
+        }
+        else if (b == 6)
+        {
+            a = false;
+        }
+    }
+
+	system("pause");
+	return 0;
+}
